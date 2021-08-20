2ad09cb64df0f0620db61821218c2a421b72e2ed
choidz <tjdgusxx@gmail.com>
2021-08-20 18:14:04 +0900

    platform start

---
+++ index.js
@@ -0,0 +1,9 @@
+function getSelectValue1(e)
+{
+ e.fakeSelect1.value = e.s_v3.options[e.s_v3.selectedIndex].value;
+}
+
+function getSelectValue2(e)
+{
+ e.fakeSelect2.value = e.s_v5.options[e.s_v5.selectedIndex].value;
+}
